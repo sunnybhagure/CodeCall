51707d3ed1b59988c96f766a23c2d5ea449a640a
Sunny Bhagure <sunnybhagure64@gmail.com>
2026-07-23 23:48:27 +0530

    update videomeet

--- Frontend/src/pages/videoMeet.jsx
+++ Frontend/src/pages/videoMeet.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { useRef } from 'react'
+import '../App.css';
+import React, { useRef, useState, useEffect } from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import "../styles/videoMeetCss.css"
 
 var connections = {}
@@ -10,7 +12,7 @@ const peerConnectionConfig = {
   ]
 }
 
-export default function videoMeet() {
+export default function VideoMeet() {
 
     var socketRef = useRef();
     let socketIdref = useRef();
@@ -45,7 +47,46 @@ export default function videoMeet() {
 
     let [videos, setVideos] = useState([]);
 
+
+    const getPermissions = async () => {
+      try {
+        const videoPermission  = await navigator.mediaDevices.getUserMedia({ video: true });
+        if(videoPermission) {
+          setVideoAvailable(true);
+        }else {
+          setVideoAvailable(false);
+        } 
+
+        const audioPermission = await navigator.mediaDevices.getUserMedia({  audio: true });
+        if(audioPermission) {
+          setAudioAvailable(true);
+        }else {
+          setAudioAvailable(false);
+        } 
+
+        if( navigator.mediaDevices.getDisplayMedia) {
+          setScreenShareAvailable(true);
+        }else {
+          setScreenShareAvailable(false);
+        }
+
+        if(videoAvailable && audioAvailable) {
+          const stream = await navigator.mediaDevices.getUserMedia({ video: videoAvailable, audio: audioAvailable });
+          
+          if(userMediaStream) {
+            window.localStream = userMediaStream;
+            if(localVideoRef.current) {
+              localVideoRef.current.srcObject = userMediaStream;
+            }
+          }
+        }
+
+      } catch (err) {
+        console.error('Error accessing media devices.', err);
+      }
+    };
     useEffect(() => {
+      getPermissions();
     }, []);
 
   return (
@@ -53,6 +94,12 @@ export default function videoMeet() {
         {askForUsername === true ? 
         <div> 
             <h2>Enter your username</h2>
+            <TextField id="outlined-basic" label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <Button variant="contained">Connect</Button>
+
+            <div>
+              <video ref={localVideoRef} autoPlay muted></video>
+              </div>
 
         </div> : <>   </>
 
